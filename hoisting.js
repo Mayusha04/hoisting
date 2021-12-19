2e820a091a686ff4fd6cb8507e65922b8c5412d7
Mayusha04 <mayushachowdary95@gmail.com>
2021-12-20 05:15:45 +0530

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,11 @@
+function myCar(name) {
+    console.log("my car name is" + " "+ name);
+
+}
+myCar("Audi");
+
+
+mydog("puppy");
+function mydog(name) {
+    console.log("my dog name is" + " "+ name);
+}
